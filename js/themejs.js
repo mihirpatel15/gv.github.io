f173d6f1489a738ab00adeeb873580902fd8e42a
mihir <mihirpatel1@ymail.con>
2020-09-26 18:21:32 +0530

    we4

--- js/themejs.js
+++ js/themejs.js
@@ -135,7 +135,23 @@ $(document).ready(function(){
 
 /* for navbar colleps */
 
-
+/* owl carosol */
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
 
 
 

b57255f31e2824b4f88f058f03e8ed22b614367a
mihir <mihirpatel1@ymail.con>
2020-09-27 11:17:57 +0530

    se

--- js/themejs.js
+++ js/themejs.js
@@ -140,16 +140,18 @@ $('.owl-carousel').owlCarousel({
     loop:false,
     margin:10,
     nav:true,
+    dots: false,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:2
         },
         1000:{
-            items:4
+            items:3
         }
+        
     }
 })
 
